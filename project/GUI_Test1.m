cd39b0490980df5d7386f5c6423a0c86ca9c73fc
parzival111 <44385243+parzival111@users.noreply.github.com>
2020-02-06 22:58:31 -0500

    gui

--- project/GUI_Test1.m
+++ project/GUI_Test1.m
@@ -624,24 +624,27 @@ function resetIR_Callback(hObject, eventdata, handles)
 % hObject    handle to resetIR (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-openArduino();
-i(1) = zeros(4,1);
-while i(1) == 0
-    i(1) = fscanf(s)
-end
-while i(2) == 0
-    i(2) = fscanf(s)
-end
-while i(3) == 0
-    i(3) = fscanf(s)
-end
-while i(4) == 0
-    i(4) = fscanf(s)
-end
-handle.irF = i(1);
-handle.irR = i(2);
-handle.irB = i(3);
-handle.irL = i(4);
+delete(instrfind({'Port'},{'COM5'}));
+s = serial('COM5');
+fopen(s);
+i = zeros(4,1);
+% while i(1) == 0
+    handle.irF = fgets(s)
+% end
+% while i(2) == 0
+%     i(2) = fscanf(s)
+% end
+% while i(3) == 0
+%     i(3) = fscanf(s)
+% end
+% while i(4) == 0
+%     i(4) = fscanf(s)
+% end
+% handle.irF = i(1);
+% handle.irR = i(2);
+% handle.irB = i(3);
+% handle.irL = i(4);
+fclose(s);
 
 % --- Executes on button press in sendPath.
 function sendPath_Callback(hObject, eventdata, handles)
@@ -658,8 +661,8 @@ function sendPos_Callback(hObject, eventdata, handles)
 
 %% Custom Functions
 function openArduino()
-delete(instrfind({'Port'},{'COM6'}));
-s = serial('COM6');
+delete(instrfind({'Port'},{'COM5'}));
+s = serial('COM5');
 fopen(s);
 
 function closeArduino()
